3e33dd1d2060fd1d645e7d5b02b79fd9bc2edb71
172697808@qq.com <172697808@qq.com>
2016-10-23 16:31:13 +0800

    first commit

---
+++ Cocoapods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cocoapods
+//
+//  Created by hy on 2016/10/23.
+//  Copyright © 2016年 hy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cocoapods/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cocoapods
+//
+//  Created by hy on 2016/10/23.
+//  Copyright © 2016年 hy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
